eb1ef038616b3de938169fc5c1ba5489ca2ab49c
Andre.Melo <andre.melo@techne.com.br>
2015-12-10 19:17:19 -0200

    Alterações para novo layout

--- gen/br/com/safadometro/activity/R.java
+++ gen/br/com/safadometro/activity/R.java
@@ -14,8 +14,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int btnCalcularData=0x7f060001;
-        public static final int btnSelecionarData=0x7f060000;
+        public static final int btnCalcularData=0x7f060003;
+        public static final int campoAno=0x7f060002;
+        public static final int campoDia=0x7f060000;
+        public static final int campoMes=0x7f060001;
     }
     public static final class layout {
         public static final int main_activity=0x7f030000;
@@ -23,7 +25,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int calcular=0x7f040002;
-        public static final int data_nascimento=0x7f040001;
+        public static final int titulo=0x7f040001;
     }
     public static final class style {
         /** 
--- src/br/com/safadometro/activity/MainActivity.java
+++ src/br/com/safadometro/activity/MainActivity.java
@@ -1,77 +1,78 @@
 package br.com.safadometro.activity;
 
-import org.joda.time.DateTime;
-
 import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.DatePickerDialog.OnDateSetListener;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.Toast;
-import br.com.safadometro.activity.R;
 
 public class MainActivity extends Activity {
 
-	//private EditText editTextData;
-	private Integer dia, mes, ano;
+	private View campoDia, campoMes, campoAno;
 	private Button btnCalcular;
 
-	private OnDateSetListener listener = new OnDateSetListener() {
-		@Override
-		public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-			StringBuilder builderDate = new StringBuilder();
-			ano = year;
-			mes = monthOfYear;
-			dia = dayOfMonth;
-			builderDate.append(dia);
-			builderDate.append("/");
-			builderDate.append(mes+1);
-			builderDate.append("/");
-			builderDate.append(ano);
-			btnCalcular.setText(builderDate);
-		
-		}
-	};
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
-		//editTextData = (EditText) findViewById(R.id.editTextData);
-		btnCalcular = (Button) findViewById(R.id.btnSelecionarData);
-		bindView();
-	}
-	
-	public void bindView(){
-		DateTime dateTime = new DateTime();
-		StringBuilder builderDate = new StringBuilder();
-		ano = dateTime.getYear();
-		mes = dateTime.getMonthOfYear();
-		dia = dateTime.getDayOfMonth();
-		builderDate.append(dia);
-		builderDate.append("/");
-		builderDate.append(mes);
-		builderDate.append("/");
-		builderDate.append(ano);
-		btnCalcular.setText(builderDate);
-	}
-	
-	public void selecionarData(View view){
-		showDialog(view.getId());
-	}
-	
-	@Override
-	protected Dialog onCreateDialog(int id) {
-		if(R.id.btnSelecionarData == id){
-			if(ano == 1988 && mes == 9 && dia == 6){
-				Toast.makeText(getBaseContext(), "SAFADO", Toast.LENGTH_LONG).show();
+		campoDia = findViewById(R.id.campoDia);
+		campoMes = findViewById(R.id.campoMes);
+		campoAno = findViewById(R.id.campoAno);
+		btnCalcular = (Button) findViewById(R.id.btnCalcularData);
+		
+		btnCalcular.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
 			}
-			return new DatePickerDialog(this, listener, ano, mes, dia);
-		}
-	   return null;
+		});
+		
 	}
+
+	
+	//	private OnDateSetListener listener = new OnDateSetListener() {
+//		@Override
+//		public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+//			StringBuilder builderDate = new StringBuilder();
+//			ano = year;
+//			mes = monthOfYear;
+//			dia = dayOfMonth;
+//			builderDate.append(dia);
+//			builderDate.append("/");
+//			builderDate.append(mes+1);
+//			builderDate.append("/");
+//			builderDate.append(ano);
+//			btnCalcular.setText(builderDate);
+//		
+//		}
+//	};
+	
+	
+//	public void bindView(){
+//		DateTime dateTime = new DateTime();
+//		StringBuilder builderDate = new StringBuilder();
+//		ano = dateTime.getYear();
+//		mes = dateTime.getMonthOfYear();
+//		dia = dateTime.getDayOfMonth();
+//		builderDate.append(dia);
+//		builderDate.append("/");
+//		builderDate.append(mes);
+//		builderDate.append("/");
+//		builderDate.append(ano);
+//		btnCalcular.setText(builderDate);
+//	}
+	
+	
+//	@Override
+//	protected Dialog onCreateDialog(int id) {
+//		if(R.id.btnSelecionarData == id){
+//			if(ano == 1988 && mes == 9 && dia == 6){
+//				Toast.makeText(getBaseContext(), "SAFADO", Toast.LENGTH_LONG).show();
+//			}
+//			return new DatePickerDialog(this, listener, ano, mes, dia);
+//		}
+//	   return null;
+//	}
+	
 }
