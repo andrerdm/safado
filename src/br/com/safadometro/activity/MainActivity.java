42aac31fd8faee3ccbd3ac4a91faf2e5a0b12c14
André <Andr@DESKTOP-4NC9GGS>
2015-12-14 23:49:47 -0200

    incluido validaçoes de campos  não finalizado ainda

--- src/br/com/safadometro/activity/MainActivity.java
+++ src/br/com/safadometro/activity/MainActivity.java
@@ -2,12 +2,15 @@ package br.com.safadometro.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -23,15 +26,28 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.main_activity);
 		
 		campoDia = (EditText) findViewById(R.id.campoDia);
+		InputFilterMinMax filtroDia = new InputFilterMinMax(1, 31);
+		campoDia.setFilters(new InputFilter[]{});
+		
 		campoMes = (EditText) findViewById(R.id.campoMes);
+		InputFilterMinMax filtroMes = new InputFilterMinMax(1, 12);
+		campoDia.setFilters(new InputFilter[]{filtroMes});
+		
 		campoAno = (EditText) findViewById(R.id.campoAno);
+		InputFilterMinMax filtroAno = new InputFilterMinMax(1, 99);
+		campoDia.setFilters(new InputFilter[]{filtroAno});
+		
 		vagabundo = (TextView) findViewById(R.id.vagabundo);
 		anjo = (TextView) findViewById(R.id.anjo);
 		btnCalcular = (Button) findViewById(R.id.btnCalcularData);		
 		btnCalcular.setOnClickListener(new OnClickListener() {		
 			@Override
 			public void onClick(View v) {
-				calcularPorcentagem(converterInt(campoDia), converterInt(campoMes), converterInt(campoAno));
+				if(verificaPreenchido(campoDia) && verificaPreenchido(campoMes) && verificaPreenchido(campoAno)){
+					calcularPorcentagem(converterInt(campoDia), converterInt(campoMes), converterInt(campoAno));					
+				}else{
+					Toast.makeText(getApplicationContext(), "Preencha todos os Campos!", Toast.LENGTH_SHORT).show();
+				}
 			}
 		});
 		
@@ -44,15 +60,12 @@ public class MainActivity extends Activity {
 	 * @param ano
 	 */
 	private void calcularPorcentagem(int dia, int mes, int ano){
-		int somatorio;
-		double safadeza, anjo, conta2;
-		float conta1;
+		Double safadeza, anjo, anoD = 0.0;
+		anoD = (double) ano;
+		anjo = 100d;
 		
-		somatorio = this.somatorio(mes);
-		conta1 = (5 / 100);
-		conta2 = (50 - dia);
-		safadeza = somatorio + conta1 * conta2;
-		anjo = 100 - safadeza;
+		safadeza = this.somatorio(mes) + (anoD / 100) * (50 - dia);
+		anjo -= safadeza;
 		
 		this.anjo.setText(anjo +" % Anjo");
 		this.vagabundo.setText(safadeza +" % Vagabundo");
@@ -83,5 +96,14 @@ public class MainActivity extends Activity {
 		
 	}
 		
-
+	private boolean verificaPreenchido(EditText campo){
+		String valor = campo.getText().toString();
+		
+		if (valor == "" || valor == null){
+			return false;			
+		}else{
+			return true;
+		}
+	}
+	
 }
